0cd1cb061b941f0e886928b78a58ae7df4390a52
BantengSerem <65397560+BantengSerem@users.noreply.github.com>
2021-06-13 21:23:20 +0700

    Update signup.js

--- JS/signup.js
+++ JS/signup.js
@@ -1,33 +1,75 @@
-const username = $("#name");
-const email = $("#email");
-const password = $("#password");
-const region = $("#region");
-const dob = $("#dob");
-const errorElement = $("#error");
+var form = document.forms["form"]
+var errorName = document.getElementById("error_username")
+var errorEmail = document.getElementById("error_email")
+var errorPassword = document.getElementById("error_password")
+var errorRegion = document.getElementById("error_region")
+var errorDOB = document.getElementById("error_DOB")
+var errorAgree = document.getElementById("error_agree")
 
-form.addEventListener('submit', function(e) {
-    let error_messages = []
-
-    // Checking the name, email, password, region, DOB
-    if (username.value === '' || username.value === null) {
-        error_messages.push("Name is required");
+function validateForm(e) {
+    var isValid = true
+    var name = document.getElementById('username').value
+    if (name == '') {
+        errorName.innerText = "*Required field"
+        isValid = false
+        e.preventDefault() // mencegah page refresh
     }
 
-    if (email.value === '' || email.value === null) {
-        error_messages.push("Email is required");
-    }
-
-    if (password.value.length > 20 || password.value.length < 8) {
-        error_messages.push("password length must be [8 - 20]")
-    }
-
-    if (dob.value === '' || dob.value === null) {
-        error_messages.push("DOB have to be filled")
-    }
-
-    // Add and show the messages
-    if (error_messages.length > 0) {
+    var menu = document.getElementById("email").value
+    if (menu == "") {
+        errorEmail.innerText = "*Required field"
+        isValid = false
         e.preventDefault()
-        errorElement.innerText = messages.joinn(', ');
+    }
+
+    var qty = document.getElementById("password").value
+    if (qty == "") {
+        errorPassword.innerText = "*Required field"
+        isValid = false
+        e.preventDefault()
+    } else if (qty.length < 8) {
+        errorPassword.innerText = "*Password must be more than 8"
+        isValid = false
+        e.preventDefault()
+    }
+
+    var size = document.getElementsByClassName("region").value
+    if (form['region'].value == "") {
+        errorRegion.innerText = "*Please choose one of the regions"
+        isValid = false
+        e.preventDefault()
+    }
+
+    var dob = document.getElementsByClassName("dob").value
+    if (dob == "") {
+        errorDOB.innerText = "*Required field"
+        isValid = false
+        e.preventDefault()
+    }
+
+    var agree = document.getElementById("agree").checked
+    if (agree == false) {
+        errorAgree.innerText = "*Must be checked"
+        isValid = false
+        e.preventDefault()
+    }
+
+    if (isValid == true) {
+        var confirmation = confirm("Do you want to submit?")
+        if (confirmation == true) {
+            alert("Thank you for your order!!!")
+        }
+    }
+}
+
+$("#menu").on("click", function() {
+    $(".drop_down").slideDown();
+});
+
+$(document).mouseup(function(e) {
+    var container = $("#menu");
+
+    if (!container.is(e.target) && container.has(e.target).length === 0) {
+        $(".drop_down").slideUp();
     }
 });
